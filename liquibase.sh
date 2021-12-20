8cbe4c633b64504472a644425ea732154edbae8f
reddybadboy <85503733+reddybadboy@users.noreply.github.com>
2021-12-20 12:44:11 +0530

    Create liquibase.sh

---
+++ liquibase.sh
@@ -0,0 +1,6 @@
+-- Script Name: singledb-deployment.sh
+#!/bin/bash
+set -x
+cd sqljdbc_9.4/enu
+liquibase --driver=com.microsoft.sqlserver.jdbc.SQLServerDriver --classpath="/home/ubuntu/sqljdbc_9.4/enu/mssql-jdbc-9.4.0.jre8.jar" --url="jdbc:sqlserver://newdatabase.c1k79dvxt7ab.us-east-2.rds.amazonaws.com:1433;databaseName=testdb1;integratedSecurity=false;" --changeLogFile="changeset.sql"  --username=admin  --password=admin1234 update  ;
+sudo rm -rf sqljdbc_9.4/enu/changset.sql
