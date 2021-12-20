146bd86338e9a6f66333e8e7d78ecdc6b22d7a11
reddybadboy <85503733+reddybadboy@users.noreply.github.com>
2021-12-20 13:42:02 +0530

    Update changeset.sql

--- changeset.sql
+++ changeset.sql
@@ -2,4 +2,4 @@
 
 --changeset liquibase:2
 --Database: testdb1
-CREATE TABLE table_12 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
+CREATE TABLE naresh (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
