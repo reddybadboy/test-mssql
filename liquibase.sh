55dac9d5a995f2a4f73e0712969c4861ac19efe6
reddybadboy <85503733+reddybadboy@users.noreply.github.com>
2021-12-21 21:43:55 +0530

    Update liquibase.sh

--- liquibase.sh
+++ liquibase.sh
@@ -3,7 +3,5 @@
 set -x
 cd sqljdbc_9.4/enu
 liquibase --driver=com.microsoft.sqlserver.jdbc.SQLServerDriver --classpath="/home/ubuntu/sqljdbc_9.4/enu/mssql-jdbc-9.4.0.jre8.jar" --url="jdbc:sqlserver://newdatabase.c1k79dvxt7ab.us-east-2.rds.amazonaws.com:1433;databaseName=testdb3;integratedSecurity=false;" --changeLogFile="changeset.sql"  --username=admin  --password=admin1234 clearCheckSums  ;
-liquibase --driver=com.microsoft.sqlserver.jdbc.SQLServerDriver --classpath="/home/ubuntu/sqljdbc_9.4/enu/mssql-jdbc-9.4.0.jre8.jar" --url="jdbc:sqlserver://newdatabase.c1k79dvxt7ab.us-east-2.rds.amazonaws.com:1433;databaseName=testdb2;integratedSecurity=false;" --changeLogFile="changeset.sql"  --username=admin  --password=admin1234 clearCheckSums  ;
 liquibase --driver=com.microsoft.sqlserver.jdbc.SQLServerDriver --classpath="/home/ubuntu/sqljdbc_9.4/enu/mssql-jdbc-9.4.0.jre8.jar" --url="jdbc:sqlserver://newdatabase.c1k79dvxt7ab.us-east-2.rds.amazonaws.com:1433;databaseName=testdb3;integratedSecurity=false;" --changeLogFile="changeset.sql"  --username=admin  --password=admin1234 update  ;
-liquibase --driver=com.microsoft.sqlserver.jdbc.SQLServerDriver --classpath="/home/ubuntu/sqljdbc_9.4/enu/mssql-jdbc-9.4.0.jre8.jar" --url="jdbc:sqlserver://newdatabase.c1k79dvxt7ab.us-east-2.rds.amazonaws.com:1433;databaseName=testdb2;integratedSecurity=false;" --changeLogFile="changeset.sql"  --username=admin  --password=admin1234 update  ;
 sudo rm -rf sqljdbc_9.4/enu/changset.sql
