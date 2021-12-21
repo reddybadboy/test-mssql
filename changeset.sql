ef10aa46df6be535709ad94563d3a6c93bf95e8d
reddybadboy <85503733+reddybadboy@users.noreply.github.com>
2021-12-21 21:21:13 +0530

    Update changeset.sql

--- changeset.sql
+++ changeset.sql
@@ -4,3 +4,5 @@
 --Database: testdb3
 CREATE TABLE hello (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
 CREATE TABLE hello_1 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
+CREATE TABLE hello_1 (test_id INT, test_id_check INT, test_column VARCHAR, PRIMARY KEY (test_id))
+
