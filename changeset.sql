6917b4d6ac964db77cc293b3597685152704e94b
reddybadboy <85503733+reddybadboy@users.noreply.github.com>
2021-12-21 22:10:17 +0530

    Update changeset.sql

--- changeset.sql
+++ changeset.sql
@@ -2,15 +2,7 @@
 
 --changeset liquibase:2
 --Database: testdb4
-CREATE TABLE employees
-( employee_id INT NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  first_name VARCHAR(30),
-  salary VARCHAR(30),
-  phone INT NOT NULL,
-  department VARCHAR(30),
-  emp_role VARCHAR(30)
-);
-INSERT into [dbo].[employees] values ('1', 'kumar' ,'arun', '1000000', '9999998888', 'devops', 'architect' );
+CREATE TABLE hello (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
+CREATE TABLE hello_1 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
 
 
