32a258032c7c178417599f1688f3ccb726669d0d
reddybadboy <85503733+reddybadboy@users.noreply.github.com>
2021-12-22 13:27:09 +0530

    Update changeset.sql

--- changeset.sql
+++ changeset.sql
@@ -6,5 +6,6 @@
 CREATE TABLE hello_5 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
 CREATE TABLE hello_7 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
 CREATE TABLE hello_6 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
+CREATE TABLE hello_8 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
 
 
