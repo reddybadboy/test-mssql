cfe4ba40ff6b9efaa3798a65b937b99bd1985c14
reddybadboy <85503733+reddybadboy@users.noreply.github.com>
2021-12-22 10:53:41 +0530

    Update changeset.sql

--- changeset.sql
+++ changeset.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
 
 --changeset liquibase:2
---Database: testdb3
+--Database: testdb4
 
-CREATE TABLE hello (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
-CREATE TABLE hello_1 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
+CREATE TABLE hello_2(test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
+CREATE TABLE hello_3 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
 
 
