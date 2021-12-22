4841d54ed862a66a4be3f681efa2ae526173613b
reddybadboy <85503733+reddybadboy@users.noreply.github.com>
2021-12-22 13:24:35 +0530

    Update changeset.sql

--- changeset.sql
+++ changeset.sql
@@ -4,10 +4,5 @@
 --Database: testdb5
 
 CREATE TABLE hello_5 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
-CREATE TABLE hello_6 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
-CREATE TABLE hello_7 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
-CREATE TABLE hello_8 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
-CREATE TABLE hello_9 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
-
 
 
